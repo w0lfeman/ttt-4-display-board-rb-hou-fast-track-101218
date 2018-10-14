ccc4be569ce9149b80d637aeb8f40e3b79b67803
dwolfe1987 <dwolfe1987@gmail.com>
2018-10-14 16:10:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 
 def display_board(board)
   
-  puts "  #{board[0]} | #{board[1]} | #{board[2]}  "
+  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "  #{board[3]} | #{board[4]} | #{baord[5]}  "
+  puts "  #{board[3]} | #{board[4]} | #{baord[5]} "
   puts "-----------"
   puts "  #{board[6]} | #{board[7]} | #{board[8]} "  
   
